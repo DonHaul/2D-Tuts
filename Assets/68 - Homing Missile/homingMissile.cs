efbec1df84e807079583f64c142060ee07376ff3
Ramiro <ramiro.animus@gmai.com>
2016-08-10 15:08:53 +0100

    Fixed

---
+++ Assets/68 - Homing Missile/homingMissile.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class homingMissile : MonoBehaviour {
+
+
+		public float speed = 5;
+		public float rotatingSpeed= 200;
+		public GameObject target;
+
+		Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+	
+				target = GameObject.FindGameObjectWithTag ("Player");
+
+				rb = GetComponent<Rigidbody2D> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+				Vector2 point2Target = (Vector2)transform.position - (Vector2)target.transform.position;
+
+				point2Target.Normalize ();
+
+				float value = Vector3.Cross (point2Target, transform.right).z;
+
+				/*
+				if (value > 0) {
+				
+						rb.angularVelocity = rotatingSpeed;
+				} else if (value < 0)
+						rb.angularVelocity = -rotatingSpeed;
+				else
+						rotatingSpeed = 0;
+*/
+
+				rb.angularVelocity = rotatingSpeed * value;
+
+
+				rb.velocity = transform.right * speed;
+
+
+	}
+
+
+		void OnTriggerEnter2D(Collider2D other)
+		{
+
+				if (other.tag != "deadly") {
+
+						Destroy (this.gameObject, 0.02f);
+
+				}
+
+		}
+}
